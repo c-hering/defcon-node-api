d2451766e0f0d071f59cbf56e5e11171dd9d54b0
An0rak <phos4es@gmail.com>
2018-03-25 14:30:51 -0400

    changed start command

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,9 +1,13 @@
 exports.geturl = (req,res) => {
   var tabletojson = require('tabletojson');
   var url;
-  typeof req.params.urlAddition === 'undefined' ?
-    url = "https://media.defcon.org/" :
-    url = "https://media.defcon.org/" + req.params.urlAddition;
+  console.log(req.params.urlAddition);
+  if(typeof req.params.urlAddition === 'undefined'){
+    url = "https://media.defcon.org/"
+  }else{
+    let addition = req.params.urlAddition.replace(/_/g, "/")
+    url = "https://media.defcon.org/" + addition;
+  }
   tabletojson.convertUrl(url).then(table => {
     table = JSON.parse(JSON.stringify(table).replace(/\s|↓/gi,"_").toLowerCase());
     res.json(table);
--- routes/routehandler.js
+++ routes/routehandler.js
@@ -1,7 +1,7 @@
 module.exports = app => {
   var controller = require('../controllers/controller');
 
-  app.route('/parseurl/:urlAddition?*')
+  app.route('/parseurl/:urlAddition?')
     .get(controller.geturl);
 
 };
